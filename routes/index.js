2bd00fcef416009f583da3e5644a48a86be1ae32
Julius Kramer <juliuskramer00@gmail.com>
2022-04-05 15:06:32 +0200

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+/* route index */
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,68 +8,71 @@ const Task = mongoose.model('Task');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Task.find()
-    .then((tasks) => {      
-      const currentTasks = tasks.filter(task => !task.completed);
-      const completedTasks = tasks.filter(task => task.completed === true);
+    Task.find()
+        .then((tasks) => {
+            const currentTasks = tasks.filter(task => !task.completed);
+            const completedTasks = tasks.filter(task => task.completed === true);
 
-      console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
-      res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send('Sorry! Something went wrong.');
-    });
+            console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
+            res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something went wrong.');
+        });
 });
 
 
 router.post('/addTask', function(req, res, next) {
-  const taskName = req.body.taskName;
-  const createDate = Date.now();
-  
-  var task = new Task({
-    taskName: taskName,
-    createDate: createDate
-  });
-  console.log(`Adding a new task ${taskName} - createDate ${createDate}`)
+    const taskName = req.body.taskName;
+    const createDate = Date.now();
 
-  task.save()
-      .then(() => { 
-        console.log(`Added new task ${taskName} - createDate ${createDate}`)        
-        res.redirect('/'); })
-      .catch((err) => {
-          console.log(err);
-          res.send('Sorry! Something went wrong.');
-      });
+    var task = new Task({
+        taskName: taskName,
+        createDate: createDate
+    });
+    console.log(`Adding a new task ${taskName} - createDate ${createDate}`)
+
+    task.save()
+        .then(() => {
+            console.log(`Added new task ${taskName} - createDate ${createDate}`)
+            res.redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something went wrong.');
+        });
 });
 
 router.post('/completeTask', function(req, res, next) {
-  console.log("I am in the PUT method")
-  const taskId = req.body._id;
-  const completedDate = Date.now();
+    console.log("I am in the PUT method")
+    const taskId = req.body._id;
+    const completedDate = Date.now();
 
-  Task.findByIdAndUpdate(taskId, { completed: true, completedDate: Date.now()})
-    .then(() => { 
-      console.log(`Completed task ${taskId}`)
-      res.redirect('/'); }  )
-    .catch((err) => {
-      console.log(err);
-      res.send('Sorry! Something went wrong.');
-    });
+    Task.findByIdAndUpdate(taskId, { completed: true, completedDate: Date.now() })
+        .then(() => {
+            console.log(`Completed task ${taskId}`)
+            res.redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something went wrong.');
+        });
 });
 
 
 router.post('/deleteTask', function(req, res, next) {
-  const taskId = req.body._id;
-  const completedDate = Date.now();
-  Task.findByIdAndDelete(taskId)
-    .then(() => { 
-      console.log(`Deleted task $(taskId)`)      
-      res.redirect('/'); }  )
-    .catch((err) => {
-      console.log(err);
-      res.send('Sorry! Something went wrong.');
-    });
+    const taskId = req.body._id;
+    const completedDate = Date.now();
+    Task.findByIdAndDelete(taskId)
+        .then(() => {
+            console.log(`Deleted task $(taskId)`)
+            res.redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something went wrong.');
+        });
 });
 
 
